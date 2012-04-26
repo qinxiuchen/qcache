ac9d051f7d42844cf56f01eecda697c5f6acb8c0
qinxiuchen <qinxiuchen@gmail.com>
2012-04-26 15:42:47 +0800

    fix

--- frozenarray.h
+++ frozenarray.h
@@ -1,22 +1,3 @@
-/*
-  +----------------------------------------------------------------------+
-  | Hidef                                                                |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 2008 The PHP Group                                     |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Authors: Gopal Vijayaraghavan <gopalv@php.net>                       |
-  +----------------------------------------------------------------------+
-*/
-
-/* $Id: frozenarray.h 320687 2011-12-08 18:37:34Z gopalv $ */
 
 #ifndef PHP_FROZENARRAY_H
 #define PHP_FROZENARRAY_H
--- php_qcache.h
+++ php_qcache.h
@@ -1,22 +1,3 @@
-/*
-  +----------------------------------------------------------------------+
-  | Hidef                                                                |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 2007 The PHP Group                                     |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Authors: Gopal Vijayaraghavan <gopalv@php.net>                       |
-  +----------------------------------------------------------------------+
-*/
-
-/* $Id: php_qcache.h 321332 2011-12-22 13:23:57Z gopalv $ */
 
 #ifndef PHP_QCACHE_H
 #define PHP_QCACHE_H
